11372d7087f7f784a9a83adf48d478e18847dc95
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-03-26 10:12:27 -0500

    Constructor con dato y siguiente.

--- taller21-260326/src/main/java/NodoNumero.java
+++ taller21-260326/src/main/java/NodoNumero.java
@@ -2,4 +2,10 @@
 public class NodoNumero {
     private int dato;              // valor que guarda el nodo
     private NodoNumero siguiente;
+    
+    //contructor
+    public NodoNumero(int dato){
+    this.dato = dato;
+    this.siguiente= null;
+    }
 }
