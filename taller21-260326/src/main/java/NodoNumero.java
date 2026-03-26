2cdd9a49138be0571607261d549151a4c300b10d
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-03-26 09:53:24 -0500

    Atributos de la clase NodoNumero y Constructor Clase NodoNumero.

---
+++ taller21-260326/src/main/java/NodoNumero.java
@@ -0,0 +1,15 @@
+
+public class NodoNumero {
+    private int dato;
+    private NodoNumero siguiente;
+    
+    //Constructor de la clase NodoNumero
+    public NodoNumero(int dato){
+        //valor que puede guardar el dato
+    this.dato = dato; 
+    
+    //Siguiente Nodo
+    this.siguiente = null;
+    }
+    
+}
