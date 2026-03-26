c1bec59b1956ffe5ae48bad1c199d620aa3ea461
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-03-26 10:57:31 -0500

    Clase Main construccion de la entrada de datos.

--- taller21-260326/src/main/java/Taller21260326.java
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 public class Taller21260326 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        //Donde se ingresa el tamaño de la lista
+        System.out.println("Ingrese el tamaño de la lista: ");
+        int n = sc.nextInt();
+        
+        //Que numero desea quitar de la lista
+        System.out.println("Ingrese el numero que desee quitar de la lista: ");
+        int quitar = sc.nextInt();
+                
     }
 }
