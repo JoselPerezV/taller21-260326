f052caf16aa0f00d6d5eca25e8a762e7427cb8e9
Jose Luis Perez Vera <est.josel.perez@unimilitar.edu.co>
2026-03-26 10:10:16 -0500

    Atributos Clase NodoNumero

--- taller21-260326/src/main/java/NodoNumero.java
+++ taller21-260326/src/main/java/NodoNumero.java
@@ -1,19 +1,5 @@
 
 public class NodoNumero {
-    private int dato;
+    private int dato;              // valor que guarda el nodo
     private NodoNumero siguiente;
-    
-    //Constructor de la clase NodoNumero
-    public NodoNumero(int dato){
-        //valor que puede guardar el dato
-    this.dato = dato; 
-    
-    //Siguiente Nodo
-    this.siguiente = null;
-    }
-    
-    //metodos Getter y Setters
-    public int getDato(){
-    return dato;
-    }
 }
